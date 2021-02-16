1364b6013c7ea96125f673afe4861ca21ee619c1
KaryaAlkan <76557512+KaryaAlkan@users.noreply.github.com>
2021-02-17 01:47:42 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManagerStarbucks = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManagerStarbucks.Save(new Customer
+            {
+                //mernis sistemin calışabimesi için dogru veriler girilmelidir
+                DateOfBirth = new DateTime(2000),
+                FirstName = "Karya",
+                LastName = "Alkan",
+                NationalityId = "62503017756"
+            });
+
+            BaseCustomerManager customerManagerNero = new NeroCustomerManager();
+            customerManagerNero.Save(new Customer
+            {
+                DateOfBirth = new DateTime(1995, 8, 4),
+                FirstName = "Karya",
+                LastName = "Alkan",
+                NationalityId = "12345678901"
+            });
+            Console.ReadLine();
+        }
+
+       
+    }
+}
